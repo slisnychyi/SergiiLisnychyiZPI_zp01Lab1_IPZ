837e81c62f5c953b71dbf315631415af6ef2b4f5
lisnychyis <ljore@ukr.net>
2023-01-11 03:22:33 +0100

    first commit

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int counter = 0;
+    [SerializeField] private Text banannasText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bananna"))
+        {
+            Destroy(collision.gameObject);
+            counter++;
+            banannasText.text = "Banannas: " + counter;
+        }
+    }
+}
